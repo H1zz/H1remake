080ef85080d5dd1ebdd00d962bdd0cd5bc5fbb8a
H1zz <s15116129@stu.edu.tw>
2022-01-09 11:44:18 +0800

    0109

--- javascript/main.js
+++ javascript/main.js
@@ -20,6 +20,26 @@ console.log(ONE);
 console.log("02");
 
 // 年份變數
-var Year = "2022";
+var Year = new Date().getFullYear();
 // 由 html 顯示資料
 document.getElementById('Year').textContent = Year;
+
+// 條件判斷 if...else 單一條件
+var Number1 = 12;
+// 顯示條件成立，執行的資料
+if (Number1<5){
+    console.log("數字小於5")
+}
+
+if (Number1>10) console.log("數字大於10");
+
+if (Number1>10)
+    console.log("數字為:"+Number1);
+
+// 顯示條件成立，執行的資料，或是不成立時，執行的資料
+var Number2 = 12;
+if ( Number1 < Number2) {
+    console.log("Number1 < Number2")
+} else {
+    console.log("Number1 < Number2 不成立")
+} 
