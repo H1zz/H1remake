861a736244441c49c6631a985ebf5ddf1596255a
H1zz <s15116129@stu.edu.tw>
2022-01-16 17:30:48 +0800

    0116

--- javascript/web.js
+++ javascript/web.js
@@ -1,16 +1,23 @@
 MenuShow2();
 function MenuShow2(){
     //  $ jQuery的變數前綴詞
+
     // 隱藏選單
     $('#MenuNavbar').hide();
+    $('#backM').hide();
 
     // 開啟選單
     $('#menuM').click( function(){
         $('#MenuNavbar').show();
+        $('#menuM').hide();
+        $('#backM').show();        
     });
     
     // 關閉選單
-    // $('#menuM').click( function(){
-    //     $('#MenuNavbar').show();
-    // });
+    $('#backM').click( function(){
+        $('#MenuNavbar').hide();
+        $('#backM').hide();
+        $('#menuM').show(); 
+    });
 }
+
